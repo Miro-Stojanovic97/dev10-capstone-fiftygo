b6068e6ef8222b2926dc1b68a6d38fbd76c92ab0
kelieberendt <kberendt@dev-10.com>
2022-08-20 17:49:19 -0500

    stubbed out fiftygo-prod.sql

---
+++ fifty-go/sql/fiftygo-prod.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS fiftygo;
+CREATE DATABASE fiftygo;
+
+USE fiftygo;
+
+CREATE TABLE `user` (
+	
+);
+
+CREATE TABLE pin (
+
+);
+
+CREATE TABLE trip (
+
+);
+
+CREATE TABLE `type` (
+
+);
+
+CREATE TABLE `role` (
+
+);
+
+CREATE TABLE user_role (
+
+);
+
+
