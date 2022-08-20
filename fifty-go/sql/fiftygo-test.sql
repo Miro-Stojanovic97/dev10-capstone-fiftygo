86154332af87ed59b168605b6ed2066aeddbd405
kelieberendt <kberendt@dev-10.com>
2022-08-20 17:45:58 -0500

    stubbed out fiftygo-test.sql

---
+++ fifty-go/sql/fiftygo-test.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS fiftygo_test;
+CREATE DATABASE fiftygo_test;
+
+USE fiftygo_test;
+
+CREATE TABLE `user` (
+	
+);
+
+CREATE TABLE pin (
+
+);
+
+CREATE TABLE trip (
+
+);
+
+CREATE TABLE `type` (
+
+);
+
+CREATE TABLE `role` (
+
+);
+
+CREATE TABLE user_role (
+
+);
+
+
