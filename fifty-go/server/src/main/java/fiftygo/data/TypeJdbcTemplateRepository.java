46d91c8e867d72fe7356ec0c7bf22426e75cba6a
alli <ageiger@dev-10.com>
2022-08-22 08:14:32 -0500

    findall, findbyid, and add implemented for trip

--- fifty-go/server/src/main/java/fiftygo/data/TypeJdbcTemplateRepository.java
+++ fifty-go/server/src/main/java/fiftygo/data/TypeJdbcTemplateRepository.java
@@ -1,25 +1,66 @@
 package fiftygo.data;
 
 import fiftygo.models.Type;
+import org.springframework.jdbc.core.JdbcTemplate;
+import fiftygo.data.mappers.TripMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class TypeJdbcTemplateRepository implements TypeRepository{
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public TypeJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public List<Type> findAll() {
-        return null;
+        final String sql = "select trip_id, trip_description, trip_start_date," +
+                " trip_end_date, transportation, trip_priority, trip_did_it, user_id" +
+                " from trip;";
+        return jdbcTemplate.query(sql, new TripMapper());
     }
 
     @Override
     public Type findById(int typeId) {
-        return null;
+        final String sql = "select trip_id, trip_description, trip_start_date," +
+                " trip_end_date, transportation, trip_priority, trip_did_it, user_id" +
+                " from trip" +
+                " where trip_id = ?;";
+        Trip trip = jdbcTemplate.query(sql, new TripMapper(), tripId).stream
+                .findFirst().orElse(null);
+
+        return trip;
     }
 
     @Override
     public Type add(Type type) {
-        return null;
+        final String sql = "insert into trip (trip_id, trip_description, trip_start_date, trip_end_date, transportation, trip_priority, trip_did_it)" +
+                " values (?,?,?,?,?,?,?);";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, trip.getTripDescription());
+            ps.setDate(2, trip.getTripStartDate());
+            ps.setDate(3, trip.getTripEndDate());
+            ps.setString(4, trip.getTransportation());
+            ps.setInt(5, getTripPriority());
+            ps.setBoolean(6, getTripDidIt());
+            return ps;
+        }, keyHolder);
+
+        if(rowsAffected <= 0){
+            return null;
+        }
+
+        trip.setTripId(keyHolder.getKey().intValue());
+        return trip;
     }
 
     @Override
--- fifty-go/server/src/main/java/fiftygo/data/mappers/TripMapper.java
+++ fifty-go/server/src/main/java/fiftygo/data/mappers/TripMapper.java
@@ -1,4 +1,18 @@
 package fiftygo.data.mappers;
 
-public class TripMapper {
+public class TripMapper implements RowMapper<Trip> {
+
+    public Trip mapRow(ResultSet resultSet, int i) {
+        Trip trip = new Trip();
+
+        trip.setTripId(resultSet.getInt("trip_id"));
+        trip.setTripDescription(resultSet.getString("trip_description"));
+        trip.setStartDate(resultSet.getDate("trip_start_date").toLocalDate());
+        trip.setEndDate(resultSet.getDate("trip_end_date").toLocalDate());
+        trip.setTransportation(resultSet.getString("transportation"));
+        trip.setTripPriority(resultSet.getInt("trip_priority"));
+        trip.setTripDidIt(resultSet.getBoolean("trip_did_it"));
+        trip.setUserId(resultSet.getInt("user_id"));
+        return trip;
+    }
 }
