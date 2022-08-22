521e990762c74928040772157a6880fff67bb59c
kelieberendt <kberendt@dev-10.com>
2022-08-22 11:23:51 -0500

    K: starting to add to CityMapper

--- fifty-go/server/src/main/java/fiftygo/data/mappers/CityMapper.java
+++ fifty-go/server/src/main/java/fiftygo/data/mappers/CityMapper.java
@@ -1,4 +1,16 @@
 package fiftygo.data.mappers;
 
-public class CityMapper {
+import fiftygo.models.City;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CityMapper implements RowMapper<City> {
+
+    @Override
+    public City mapRow(ResultSet rs, int rowNum) throws SQLException {
+        City city = new City();
+        return null;
+    }
 }
