bc1a6508f1f021757f6b43738178817015ce2185
kelieberendt <kberendt@dev-10.com>
2022-08-22 11:28:12 -0500

    K: working on find methods

--- fifty-go/server/src/main/java/fiftygo/data/PinJdbcTemplateRepository.java
+++ fifty-go/server/src/main/java/fiftygo/data/PinJdbcTemplateRepository.java
@@ -1,6 +1,8 @@
 package fiftygo.data;
 
+import fiftygo.data.mappers.CityMapper;
 import fiftygo.data.mappers.PinMapper;
+import fiftygo.models.City;
 import fiftygo.models.Pin;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -16,19 +18,24 @@ public class PinJdbcTemplateRepository implements PinRepository{
 
     @Override
     public List<Pin> findAll() {
-        final String sql = "select pin_id, pin_description, pin_date, pin_priority, pin_did_it, user_id from pin;";
-
+        final String sql = "";
         return jdbcTemplate.query(sql, new PinMapper());
     }
 
     @Override
-    public List<Pin> findByUsername(String username) {
-        return null;
+    public List<Pin> findByUserId(int userId) {
+        final String sql = "select pin_id, pin_description, pin_date, pin_priority, pin_did_it, user_id " +
+                "from pin " +
+                "where user_id = ?;";
+        return jdbcTemplate.query(sql, new PinMapper(), userId);
     }
 
     @Override
     public Pin findById(int pinId) {
-        return null;
+        final String sql = "select pin_id, pin_description, pin_date, pin_priority, pin_did_it, user_id " +
+                "from pin " +
+                "where pin_id = ?;";
+        return jdbcTemplate.queryForObject(sql, new PinMapper(), pinId);
     }
 
     @Override
@@ -45,4 +52,11 @@ public class PinJdbcTemplateRepository implements PinRepository{
     public boolean deleteById(int pinId) {
         return false;
     }
+
+    private void addCity(Pin pin) {
+        final String sql = "";
+        City city = jdbcTemplate.queryForObject(sql, new CityMapper(), pin.getPinId());
+        pin.setCity(city);
+    }
 }
+
