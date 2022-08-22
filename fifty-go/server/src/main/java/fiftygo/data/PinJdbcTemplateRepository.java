8244d8c732346c234f6a13eff20f5978735e119a
kelieberendt <kberendt@dev-10.com>
2022-08-21 22:02:11 -0500

    K: set PinJdbcRepo to implement PinRepo, imported empty methods from PinRepo

--- fifty-go/server/src/main/java/fiftygo/data/PinJdbcTemplateRepository.java
+++ fifty-go/server/src/main/java/fiftygo/data/PinJdbcTemplateRepository.java
@@ -1,4 +1,37 @@
 package fiftygo.data;
 
-public class PinJdbcTemplateRepository {
+import fiftygo.models.Pin;
+
+import java.util.List;
+
+public class PinJdbcTemplateRepository implements PinRepository{
+    @Override
+    public List<Pin> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Pin> findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public Pin findById(int pinId) {
+        return null;
+    }
+
+    @Override
+    public Pin add(Pin pin) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Pin pin) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(int pinId) {
+        return false;
+    }
 }
