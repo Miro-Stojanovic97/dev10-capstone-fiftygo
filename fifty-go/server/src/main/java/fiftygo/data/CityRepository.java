93291f3b332b5018b68299f227faf19ff16f1d1c
kelieberendt <kberendt@dev-10.com>
2022-08-21 22:09:37 -0500

    K: filled in CityRepo and CityJdbcRepo with empty methods

--- fifty-go/server/src/main/java/fiftygo/data/CityRepository.java
+++ fifty-go/server/src/main/java/fiftygo/data/CityRepository.java
@@ -1,4 +1,21 @@
 package fiftygo.data;
 
+import fiftygo.models.City;
+import fiftygo.models.Pin;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 public interface CityRepository {
+
+    List<City> findAll();
+
+    City findById(int cityId);
+
+    City add(City city);
+
+    boolean update(City city);
+
+    @Transactional
+    boolean deleteById(int cityId);
 }
