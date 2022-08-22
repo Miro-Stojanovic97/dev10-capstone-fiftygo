d439eafbf68b39d6c940442110c98200e57c6e7a
kelieberendt <kberendt@dev-10.com>
2022-08-21 22:00:57 -0500

    K: set TripJdbcRepo to implement Trip Repo, imported blank methods from TripRepo

--- fifty-go/server/src/main/java/fiftygo/data/TripJdbcTemplateRepository.java
+++ fifty-go/server/src/main/java/fiftygo/data/TripJdbcTemplateRepository.java
@@ -1,14 +1,18 @@
 package fiftygo.data;
 
+import fiftygo.models.Trip;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-public class TripJdbcTemplateRepository {
+import java.util.List;
+
+public class TripJdbcTemplateRepository implements TripRepository{
 
     private final JdbcTemplate jdbcTemplate;
 
     public TripJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
+
 
 //    private final RowMapper<Trip> mapper = (resultSet, rowIndex) -> {
 //        Trip trip = new Trip();
@@ -21,4 +25,35 @@ public class TripJdbcTemplateRepository {
 //
 //        final String sql = "select * from trip"
 //    }
+
+
+    @Override
+    public List<Trip> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Trip> findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public Trip findById(int tripId) {
+        return null;
+    }
+
+    @Override
+    public Trip add(Trip trip) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Trip trip) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(int tripId) {
+        return false;
+    }
 }
