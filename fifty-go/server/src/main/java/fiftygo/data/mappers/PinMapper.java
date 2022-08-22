1a97e41c3264bb74117126c61078b88e6d3acac9
kelieberendt <kberendt@dev-10.com>
2022-08-22 09:58:29 -0500

    K: filled in PinMapper

--- fifty-go/server/src/main/java/fiftygo/data/mappers/PinMapper.java
+++ fifty-go/server/src/main/java/fiftygo/data/mappers/PinMapper.java
@@ -1,4 +1,24 @@
 package fiftygo.data.mappers;
 
-public class PinMapper {
+import fiftygo.models.Pin;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PinMapper implements RowMapper<Pin> {
+
+    @Override
+    public Pin mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Pin pin = new Pin();
+        pin.setPinId(rs.getInt("pin_id"));
+        pin.setPinDescription(rs.getString("pin_description"));
+        if (rs.getDate("pin_date") != null) {
+            pin.setPinDate(rs.getDate("pin_date").toLocalDate());
+        }
+        pin.setPinPriority(rs.getInt("pin_priority"));
+        pin.setPinDidIt(rs.getBoolean("pin_did_it"));
+        pin.setUserId(rs.getInt("user_id"));
+        return pin;
+    }
 }
