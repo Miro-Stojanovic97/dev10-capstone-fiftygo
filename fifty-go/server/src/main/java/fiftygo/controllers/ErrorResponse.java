1860ecf55bd090c1fea884ceb9063313867a488d
kelieberendt <kberendt@dev-10.com>
2022-08-23 14:07:46 -0500

    k- created ErrorResponse Class in controllers pkg

--- fifty-go/server/src/main/java/fiftygo/controllers/ErrorResponse.java
+++ fifty-go/server/src/main/java/fiftygo/controllers/ErrorResponse.java
@@ -1,4 +1,41 @@
 package fiftygo.controllers;
 
+import fiftygo.domain.Result;
+import fiftygo.domain.ResultType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ErrorResponse {
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final String message;
+
+    public String getTimestamp() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+        return timestamp.format(formatter);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public static ResponseEntity<ErrorResponse> build(String message) {
+        return new ResponseEntity<>(new ErrorResponse(message), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public static <T> ResponseEntity<Object> build(Result<T> result) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        if (result.getResultType() == null || result.getResultType() == ResultType.INVALID) {
+            status = HttpStatus.BAD_REQUEST;
+        } else if (result.getResultType() == ResultType.NOT_FOUND) {
+            status = HttpStatus.NOT_FOUND;
+        }
+        return new ResponseEntity<>(result.getErrorMessages(), status);
+    }
 }
