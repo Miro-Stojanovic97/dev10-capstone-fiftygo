781d0f607312717c1ad7f63fd254538e716a9ab6
kelieberendt <kberendt@dev-10.com>
2022-08-22 23:32:47 -0500

    K: tested findAll() for cities. passes.

---
+++ fifty-go/server/src/test/java/fiftygo/data/CityJdbcTemplateRepositoryTest.java
@@ -0,0 +1,33 @@
+package fiftygo.data;
+
+import fiftygo.models.City;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class CityJdbcTemplateRepositoryTest {
+
+    @Autowired
+    CityJdbcTemplateRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
+    }
+
+    @Test
+    void shouldFindAllCities() {
+        List<City> cities = repository.findAll();
+        assertNotNull(cities);
+        assertEquals(349, cities.size());
+    }
+}
