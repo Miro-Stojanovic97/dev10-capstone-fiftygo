b57e7c8941443d443d415f68c7e3e8ca8c885430
kelieberendt <kberendt@dev-10.com>
2022-08-24 10:59:46 -0500

    changed base-url to http://localhost:8080/fiftygo, so changed requestMapping accordingly

--- fifty-go/server/src/main/java/fiftygo/controllers/PinController.java
+++ fifty-go/server/src/main/java/fiftygo/controllers/PinController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin//(origins = {"http://localhost:3000"})
-@RequestMapping("/api/pin")
+@RequestMapping("/fiftygo/pin")
 public class PinController {
 
     private final PinService service;
--- fifty-go/server/src/main/java/fiftygo/controllers/TypeController.java
+++ fifty-go/server/src/main/java/fiftygo/controllers/TypeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/type")
+@RequestMapping("/fiftygo/type")
 public class TypeController {
 
     private final TypeService service;
