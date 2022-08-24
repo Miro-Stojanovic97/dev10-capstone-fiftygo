b5b3e7bee59f69516dd4fc71ce81ee61b1ed2b6d
kelieberendt <kberendt@dev-10.com>
2022-08-24 11:00:53 -0500

    k- updated requestMapping to work like the rest

--- fifty-go/server/src/main/java/fiftygo/controllers/TripController.java
+++ fifty-go/server/src/main/java/fiftygo/controllers/TripController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/trip")
+@RequestMapping("/fiftygo/trip")
 public class TripController {
 
     private final TripService service;
