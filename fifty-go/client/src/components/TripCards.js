9639ea0f40c719b76863b3643709c190e7d7d479
alli <ageiger@dev-10.com>
2022-08-30 08:35:51 -0500

    moved trip buttons

--- fifty-go/client/src/components/TripCards.js
+++ fifty-go/client/src/components/TripCards.js
@@ -100,7 +100,7 @@ function TripCards() {
                                                 </ul>
                                             </div>
                                             <div className='card-footer'>
-                                              <div className="col-12 py-1">
+                                              <div className="col-6 offset-4 py-1">
                                                 {auth.user && auth.user.appUserId && (
                                                 <Link className="btn btn-primary btn-sm me-1" to={`/trip/edit/${trip.tripId}`}>
                                                     <i className="bi bi-pencil-square"></i> Edit Trip
