d8e0ffaa51c8d6ed6637fc1ce6b71cf0705edf14
kelieberendt <kberendt@dev-10.com>
2022-08-29 20:33:42 -0500

    can completely update Pins!!!!!!!!

--- fifty-go/client/src/components/PinForm.js
+++ fifty-go/client/src/components/PinForm.js
@@ -3,7 +3,6 @@ import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
 import { Link, useHistory, useParams } from 'react-router-dom';
 
 import AuthContext from '../contexts/AuthContext';
-import CityList from './CityList';
 import Errors from './Errors';
 
 const PIN_DEFAULT = {
@@ -17,24 +16,17 @@ const PIN_DEFAULT = {
     appUserId: 0
 };
 
+const STATE_DEFAULT = "AK";
+
 function PinForm() {
   const [pin, setPin] = useState(PIN_DEFAULT);
   const [errors, setErrors] = useState([]);
   const [types, setTypes] = useState([]);
-  // const [type, setType] = useState({});
-  const [stateChoice, setStateChoice] = useState("");
+  const [stateChoice, setStateChoice] = useState(STATE_DEFAULT);
   const [cities, setCities] = useState([]);
-  // const [city, setCity] = useState({});
 
   const auth = useContext(AuthContext);
-
   const history = useHistory();
-
-  // Not using destructuring...
-  // const params = useParams();
-  // const id = params.id;
-
-  // Using destructuring...
   const { id } = useParams();
 
   const initGET = {
@@ -56,20 +48,11 @@ function PinForm() {
     })
     .then(data => setTypes(data))
     .catch(console.log);
-  }, []);
+  }, []); // only do this once when the page loads.
 
   useEffect(() => {
     // Make sure that we have an "id" value...
     if (id) {
-
-      // const init = {
-      //   method: 'GET',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //     'Authorization': `Bearer ${auth.user.token}`
-      //   },
-      // };
-
       fetch(`http://localhost:8080/fiftygo/pin/${id}`, initGET)
         .then(response => {
           if (response.status === 200) {
@@ -84,26 +67,6 @@ function PinForm() {
     }
   }, [id]); // Hey React... please call my arrow function every time the "id" route parameter changes value
 
-  const handleChangeType = async (event) => {
-    const type = await fetch(`http://localhost:8080/fiftygo/type/${event.target.value}`, initGET)
-      .then(response => {
-        if (response.status === 200) {
-          return response.json();
-        } else {
-          return Promise.reject(`Unexpected status code: ${response.status}`);
-        }
-      })
-      // .then(data => setType(data))
-      // .then(console.log(type))
-      .catch(console.log);
-  
-      const newPin = {...pin}
-    console.log(type);
-    newPin.type = type;
-    console.log(newPin);
-    setPin(newPin);
-  }
-
   useEffect(() => {
     // if the US state name changes in the form, get a different list of cities based on that state.
     fetch(`http://localhost:8080/fiftygo/city/state/${stateChoice}`, initGET)
@@ -117,38 +80,16 @@ function PinForm() {
       .then(data => setCities(data))
       // .then(console.log(pin))
       .catch(console.log);
-  }, [stateChoice]); // hey react, do this whenever stateChoice changes?
+  }, [stateChoice]); // hey react, do this fetch and stuff whenever stateChoice changes!
 
   const handleChangeState = (event) => {
     setStateChoice(event.target.value);
-  }
-  const handleChangeCity = async (event) => { // uses cityId to fetch specifice city
-    
-    const city = await fetch(`http://localhost:8080/fiftygo/city/${event.target.value}`, initGET)
-    .then(response => {
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        return Promise.reject(`Unexpected status code: ${response.status}`);
-      }
-    })
-    // .then(data => console.log(data))
-    // .then(console.log(pin))
-    .catch(console.log);
-  
-    pin.city = city;
-    console.log(pin);
-    // const newCity = { ...city}
-    // newCity[event.target.name] = event.target.value;
-    // setCity(newCity);
   }
 
   const handleChange = (event) => {
     // Make a copy of the object.
     const newPin = { ...pin };
 
-    // Update the value of the property that just changed.
-    // We can "index" into the object using square brackets (just like we can do with arrays).
     if (event.target.type === 'checkbox') {
       newPin[event.target.name] = event.target.checked;
     } else {
@@ -160,10 +101,8 @@ function PinForm() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(pin);
 
     if (id) {
-      //console.log(pin);
       updatePin();
     } else {
       addPin();
@@ -190,51 +129,16 @@ function PinForm() {
       })
       .then(data => {
         if (data.id) {
-          /*
-
-          On the happy path, "data" is an object that looks this:
-
-          {
-            const PIN_DEFAULT = {
-                pinDescription: "dgsdgsdg",
-                pinType: "Ski",
-                pinDate: "2022-12-12",
-                pinPriority: 5,
-                pinDidIt: 1,
-                pinCity: "Milwaukee",
-                pinState: "WI"
-};
-          }
-
-          */
-
           // Send the user back to the list route.
           history.push('/pinlist');
         } else {
-          /*
-
-          On the unhappy path, "data" is an array that looks this:
-
-          [
-            "Pin `section` is required.",
-            "Pin `row` must be a positive number less than or equal to 250.",
-            "Pin `column` must be a positive number less than or equal to 250.",
-            "Pin `material` is required."
-          ]
-
-          */
-
-          setErrors(data);
+          setErrors(data); // can get validation error messages from server
         }
       })
       .catch(console.log);
   };
 
   const updatePin = () => {
-    // assign an ID (this is probably needed anymore)
-    pin.pinId = id * 1;
-    console.log(pin);
-    console.log(JSON.stringify(pin));
 
     const init = {
       method: 'PUT',
@@ -271,21 +175,6 @@ function PinForm() {
       <h2 className="mb-4">{id ? 'Update Pin' : 'Add Pin'}</h2>
 
       <Errors errors={errors} />
-
-
-      {/* const PIN_DEFAULT = {
-          pinDescription: "",
-          pinType: "",
-          pinDate: "",
-          pinPriority: 0,
-          pinDidIt: 0,
-          city: {
-
-          },
-          type: {
-
-          }
-          }; */}
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="pinDescription">Description:</label>
@@ -295,11 +184,11 @@ function PinForm() {
         <div className="form-group">
           <label htmlFor="typeName">Type:</label>
           <select
-            id="typeName"
-            name="typeName"
+            id="typeId"
+            name="typeId"
             type="text"
             className="form-control"
-             onChange={handleChangeType}>
+             onChange={handleChange}>
               {types.map(type => (
                 <option key={type.typeId} value={type.typeId}>{type.typeName}</option>
               ))}
@@ -378,9 +267,9 @@ function PinForm() {
           </select>
         </div>
         <div className="form-group">
-          <label htmlFor="cityName">City:</label>
-          <select id="cityName" name="cityName" className="form-control"
-             onChange={handleChangeCity} >
+          <label htmlFor="cityId">City:</label>
+          <select id="cityId" name="cityId" className="form-control"
+              onChange={handleChange} >
               {/* {console.log(cities)} */}
               {cities.map(city => (
                 <option key={city.cityId} value={city.cityId}>{city.cityName}</option>
