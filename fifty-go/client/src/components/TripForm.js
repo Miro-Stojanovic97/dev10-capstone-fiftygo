6f2d86e60b565dc5b81c527e60a65f4d0acb3e2f
kelieberendt <kberendt@dev-10.com>
2022-08-30 09:18:31 -0500

    trips add edits

--- fifty-go/client/src/components/TripForm.js
+++ fifty-go/client/src/components/TripForm.js
@@ -105,6 +105,9 @@ function TripForm() {
 
 
   const addTrip = () => {
+    console.log(trip);
+    auth.user.appUserId ? trip.appUserId = auth.user.appUserId : setErrors("Need a userId!");
+
     const init = {
       method: 'POST',
       headers: {
@@ -146,7 +149,7 @@ function TripForm() {
           */
 
           // Send the user back to the list route.
-          history.push('/trip');
+          history.push('/tripcards');
         } else {
           /*
           On the unhappy path, 
