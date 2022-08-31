4697baf276a97c0739216d0a4a777f716598eed4
kelieberendt <kberendt@dev-10.com>
2022-08-31 14:46:45 -0500

    big fixes to pinForm!

--- fifty-go/client/src/components/PinForm.js
+++ fifty-go/client/src/components/PinForm.js
@@ -5,48 +5,31 @@ import AuthContext from '../contexts/AuthContext';
 import Errors from './Errors';
 
 const PIN_DEFAULT = {
-    pinId: 1,
-    pinDescription: "",
-    pinDate: "",
-    pinPriority: 0,
-    pinDidIt: false,
-    cityId: 0,
-    typeId: 0,
-    appUserId: 0
-};
-
-const TYPE_DEFAULT = {
+  pinId: 1,
+  pinDescription: "",
+  pinDate: "",
+  pinPriority: 0,
+  pinDidIt: false,
+  city: {
+    stateAbr: "NJ",
+    cityName: "Trenton",
+  },
   typeId: 0,
-  typeName: ""
-}
-
-const CITY_DEFAULT = {
-  cityId: 0,
-  cityName: "Milwaukee",
-  stateAbr: "WI",
-  stateName: "Wisconsin",
-  latitude: 43.0642,
-  longitude: -87.9675
-}
-
-const STATE_DEFAULT = CITY_DEFAULT.stateAbr;
+  appUserId: 0,
+};
 
 function PinForm() {
   const [pin, setPin] = useState(PIN_DEFAULT);
-  const [pinType, setPinType] = useState(TYPE_DEFAULT);
-  //const [city, setCity] = useState(CITY_DEFAULT);
   const [errors, setErrors] = useState([]);
   const [types, setTypes] = useState([]);
   const [states, setStates] = useState([]);
   const [stateChoice, setStateChoice] = useState({});
   const [cities, setCities] = useState([]);
-  const [currentCity, setCurrentCity] = useState({});
-  const [currentState, setCurrentState] = useState({});
+  const [cityChoice, setCityChoice] = useState({});
 
   const auth = useContext(AuthContext);
   const history = useHistory();
   const { id } = useParams();
-
 
   const initGET = {
         method: "GET",
@@ -56,24 +39,8 @@ function PinForm() {
         }
       }
 
-
-// const getPinType = () => { // get the current pin's type and store data in type via setType??
-//     fetch(`http://localhost:8080/fiftygo/type/${pin.typeId}`, initGET)
-//     .then(response => {
-//       if (response.status === 200) {
-//         return response.json();
-//       } else {
-//         return Promise.reject(`Unexpected status code: ${response.status}`);
-//       }
-//     })
-//     .then(data => setPinType(data))
-//     .catch(console.log);
-//   } // want to set type so we can have it in the form...
-
-
-useEffect(() => { // get the current pin we'd like to edit
+  useEffect(() => { // get the current pin we'd like to edit
     // Make sure that we have an "id" value...
-    //console.log(pin);
     if (id) {
       fetch(`http://localhost:8080/fiftygo/pin/${id}`, initGET)
         .then(response => {
@@ -86,7 +53,7 @@ useEffect(() => { // get the current pin we'd like to edit
         .then(data => setPin(data))
         .catch(console.log);
     }
-  }, [id]); // Hey React... please call my arrow function every time the "id" route parameter changes value
+  }, [id]); // call every time the "id" route parameter changes value
 
 
   useEffect(() => { // get all the types and store data in types via setTypes
@@ -113,63 +80,41 @@ useEffect(() => { // get the current pin we'd like to edit
     })
     .then(data => setStates(data))
     .catch(console.log);
-  }, []); // only do this when the page loads. This list doesn't need to be reloaded.
-
-  // useEffect(() => { //if stateChoice changes, please update currentCity
-  //   fetch(`http://localhost:8080/fiftygo/city/state/${stateChoice}`, initGET)
-  //   .then(response => {
-  //     if (response.status === 200) {
-  //       return response.json();
-  //     } else {
-  //       return Promise.reject(`Unexpected status code: ${response.status}`);
-  //     }
-  //   })
-  //   .then(data => setCurrentCity(data))
-  //   .catch(console.log);
-
-
-  // }, [stateChoice])
-
-
-  // make a useEffect to get currentState? use {currentCity.stateAbr}
-  useEffect(() => {
-    if (currentCity) {
-    setCurrentState(currentCity.stateAbr);
-    }
-  }, [currentCity]) // when there's a current city, or if current city changes, gimme a new current state.
+  }, []); // only do this when the page loads. 
 
   useEffect(() => {
-    if (stateChoice) {
-      setCurrentState(stateChoice);
-    }
-  }, [stateChoice])
-  
-  useEffect(() => {
-    // if the US state name changes in the form, get a different list of cities based on that state.
-    if (currentState) {
-      //console.log("currentState", currentState);
-      //console.log(initGET);
-      fetch(`http://localhost:8080/fiftygo/city/state/${currentState}`, initGET)
-      .then(response => {
+    getCitiesByState(pin.city.stateAbr);
+  }, [stateChoice]);
+
+  const getCitiesByState = (stateAbr) => {
+    fetch(`http://localhost:8080/fiftygo/city/state/${stateAbr}`, initGET)
+      .then((response) => {
         if (response.status === 200) {
           return response.json();
         } else {
           return Promise.reject(`Unexpected status code: ${response.status}`);
         }
       })
-      .then(data => setCities(data))
+      .then((data) => setCities(data))
       .catch(console.log);
-    } else {
-      //setErrors("uh oh no cities loaded.");
-    }
-    
-  }, [currentState]); // if the currentState changes, gimme a new list of cities.
-
+  };
 
   const handleChangeState = (event) => {
-    setStateChoice(event.target.value);
+    setStateHelper(event.target.value).then(() =>
+      getCitiesByState(event.target.value)
+    );
   }
 
+  const setStateHelper = (stateAbbr) => {
+    return new Promise((resolve) => {
+      setStateChoice(stateAbbr);
+      resolve();
+    });
+  };
+
+  const handleChangeCity = (event) => {
+    setCityChoice(event.target.value);
+  };
 
   const handleChange = (event) => {
     // Make a copy of the object.
@@ -183,7 +128,6 @@ useEffect(() => { // get the current pin we'd like to edit
 
     setPin(newPin);
   };
-
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -217,8 +161,7 @@ useEffect(() => { // get the current pin we'd like to edit
       })
       .then(data => {
         if (data.pinId) {
-          // Send the user back to the list route.
-          history.push('/pinlist');
+          history.push('/pinlist'); // Send the user back to the list route.
         } else {
           setErrors(data); // can get validation error messages from server
         }
@@ -228,7 +171,6 @@ useEffect(() => { // get the current pin we'd like to edit
 
 
   const updatePin = () => {
-
     const init = {
       method: 'PUT',
       headers: {
@@ -270,9 +212,6 @@ useEffect(() => { // get the current pin we'd like to edit
     } 
   }
 
-// need a way to get teh current state based on the currentCity.stateAbr
-// currentState == currentCity.stateAbr
-
   const stateMapper = (currentStateAbr) => {
     if (currentStateAbr) {
 
@@ -297,52 +236,16 @@ useEffect(() => { // get the current pin we'd like to edit
           return Promise.reject(`Unexpected status code: ${response.status}`);
         }
       })
-      .then(data => setCurrentCity(data))
+      .then(data => setCityChoice(data))
       .catch(console.log);
     }
   }, [pin])
 
-  // What I'd LIKE to have happen:
-    // use pin.cityId to get a whole cityObj.
-    // use that cityObj to setStateChoice(cityObj.stateAbr)
-    // use the stateChoice to setCities(stateChoice)
-    // then do the method where we rearrange the List<City> to have the current pin's city at the top as the default.
-
   const cityMapper = () => { // pass in pin.cityId
-    // if (currentCityId != 0) {
-    //   console.log("currentState", currentState);
-    //   console.log("currentCityId: ", currentCityId) //make sure we are getting the cityId. We are.
-    //   console.log("currentCity before if: ", currentCity)
-    //   if (currentCity.stateAbr == stateChoice && cities.length > 0) {// only actually do this if we really have a current City in state
-    //     console.log("currentCity inside if:", currentCity); // make sure we still get the right city (we aren't getting anything!! >:( )
-    //     const newCitiesArr = cities.filter(city => city.stateAbr !== currentCity.stateAbr)
-    //     console.log("cities", cities);
-      
-    //     newCitiesArr.unshift(cities.filter(city => city.stateAbr == currentCity.stateAbr)[0]);
-
-    //     console.log("newCitiesArr", newCitiesArr);
-        
-    //     const citiesArr = newCitiesArr.map(city => <option key={city.cityId} value={city.cityId}>{city.cityName}</option> )
-        
-    //     return citiesArr;
-    //   } else { //currentCity doesn't match stateChoice, just give me cities
+    if (cities.length > 0) {
         return cities.map(city => <option key={city.cityId} value={city.cityId}>{city.cityName}</option> );
-      
-
-      // then if we had a cityObj, we'd use its stateAbr to get all cities from that state
-      
-          // The rest fails because we aren't getting a city...
-      
-      //const newCitiesArr = cities.filter(city => city.cityId != currentCityId);
-      //console.log(newCitiesArr);
-      //newCitiesArr.unshift(cities.filter(city => city.cityId == currentCityId)[0]);
-
-      //const citiesArr = newCitiesArr.map(city => <option key={city.cityId} value={city.cityId}>{city.cityName}</option> )
-      
-      
-      // return citiesArr;
-    
-  }
+    }
+  };
 
   return (
     <>
@@ -380,7 +283,7 @@ useEffect(() => { // get the current pin we'd like to edit
         <div className="form-group">
           <label htmlFor="stateChoice">State:</label>
           <select id="stateChoice" name="stateChoice" className="form-control" onChange={handleChangeState}>
-              {stateMapper(currentCity.stateAbr ? currentCity.stateAbr : "")}
+              {stateMapper(cityChoice.stateAbr ? cityChoice.stateAbr : "")}
           </select>
         </div>
         <div className="form-group">
