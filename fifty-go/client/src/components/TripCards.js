bb13157caafe11bc4916b2351a616620347f03d6
kelieberendt <kberendt@dev-10.com>
2022-09-01 14:53:40 -0500

    adjusted margin spacing on tripdcards page

--- fifty-go/client/src/components/TripCards.js
+++ fifty-go/client/src/components/TripCards.js
@@ -100,64 +100,69 @@ function TripCards() {
 
 
     return (
-        <>
-                <div className="container">
-                    <h2 className="mt-4">{auth.user.firstName}'s Trips</h2>
-                    <button className="btn btn-primary my-4" onClick={() => history.push('/trip/add')}>
-                        <i className="bi bi-plus-circle"></i> Add Trip
-                    </button>
-                    </div>
-                    <div className="container">
-                        <div className="row">
-                            {trips.map(trip => (
-                                <div key={trip.tripId + "-key"} className="col-10 offset-1 card mb-5">
-                                        <div className="card-body">
-                                            <h5 className="card-title mb-3">{trip.tripDescription}</h5>
-                                            <div className="col-12">
-                                                <h6 className="card-subtitle mb-2">Start date: {trip.tripStartDate}</h6>
-                                                <h6 className="card-subtitle mb-2">End date: {trip.tripEndDate}</h6>
-                                            </div>
-                                            <div className="col-6">
-                                                <h6 className="card-subtitle mb-2">Priority: {trip.tripPriority}</h6>
-                                            </div>
-                                            <div className="col-6">
-                                                <h6 className="card-subtitle mb-2">{trip.tripDidIt ? 'Completed this trip!' : 'Have not completed this trip.'}</h6>
-                                            </div>
-                                            <div className="col-12 pt-4">
-                                                <h6 className="card-text mb-2">There are {trip.pins.length} Pins in this Trip:</h6>
-                                                <ul className="list-group list-group-flush">
-                                                {trip.pins.map(pin => (
-                                                    <li key={pin.pinId} className="list-group-item">
-                                                      <h6><strong>{pin.type.typeName}:</strong> {pin.pinDescription}</h6>
-                                                      <p>{pin.pinDate}</p>
-                                                    <Link className="btn btn-primary btn-sm me-2 mb-1" to={`/pins/edit/${pin.pinId}`}>
-                                                      <i className="bi bi-pencil-square"></i> Edit Pin
-                                                    </Link>
-                                                    <button onClick={() => removePinFromTrip(pin.pinId, trip.tripId)} className="btn btn-danger btn-sm me-2 mb-1">Remove this from the Trip</button>
-                                                    </li>
-                                                  ))}
-                                                </ul>
-                                            </div>
-                                            <div className='card-footer'>
-                                              <div className="edit-trip py-1">
-                                                {auth.user && auth.user.appUserId && (
-                                                <Link className="btn btn-primary btn-sm me-1" to={`/trip/edit/${trip.tripId}`}>
-                                                    <i className="bi bi-pencil-square"></i> Edit Trip
-                                                </Link>
-                                                )}
-                                                {auth.user && auth.user.hasRole('ROLE_ADMIN') && (
-                                                <button className="btn btn-danger btn-sm" onClick={() => handleDeleteTrip(trip)}>
-                                                    <i className="bi bi-trash"></i> Delete Trip
-                                                </button>
-                                                )}
-                                              </div>
-                                            </div> 
-                                        </div>
-                                </div>   
-                            ))}
-                        </div>
-                    </div>
-        </>
+        
+    
+      <div className='container offset-1 p-5'>
+        <div className='container p-3'>
+          <h2>{auth.user.firstName}'s Trips</h2>
+        <button className="btn btn-primary my-4 col-1" onClick={() => history.push('/trip/add')}>
+            <i className="bi bi-plus-circle"></i> Add Trip
+        </button>
+        </div>
+        
+      
+        
+            
+              
+                {trips.map(trip => (
+                    <div key={trip.tripId + "-key"} className="col-10 card mb-5">
+                            <div className="card-body">
+                                <h5 className="card-title mb-3">{trip.tripDescription}</h5>
+                                <div className="col-12">
+                                    <h6 className="card-subtitle mb-2">Start date: {trip.tripStartDate}</h6>
+                                    <h6 className="card-subtitle mb-2">End date: {trip.tripEndDate}</h6>
+                                </div>
+                                <div className="col-6">
+                                    <h6 className="card-subtitle mb-2">Priority: {trip.tripPriority}</h6>
+                                </div>
+                                <div className="col-6">
+                                    <h6 className="card-subtitle mb-2">{trip.tripDidIt ? 'Completed this trip!' : 'Have not completed this trip.'}</h6>
+                                </div>
+                                <div className="col-12 pt-4">
+                                    <h6 className="card-text mb-2">There are {trip.pins.length} Pins in this Trip:</h6>
+                                    <ul className="list-group list-group-flush">
+                                    {trip.pins.map(pin => (
+                                        <li key={pin.pinId} className="list-group-item">
+                                          <h6><strong>{pin.type.typeName}:</strong> {pin.pinDescription}</h6>
+                                          <p>{pin.pinDate}</p>
+                                        <Link className="btn btn-primary btn-sm me-2 mb-1" to={`/pins/edit/${pin.pinId}`}>
+                                          <i className="bi bi-pencil-square"></i> Edit Pin
+                                        </Link>
+                                        <button onClick={() => removePinFromTrip(pin.pinId, trip.tripId)} className="btn btn-danger btn-sm me-2 mb-1">Remove this from the Trip</button>
+                                        </li>
+                                      ))}
+                                    </ul>
+                                </div>
+                                <div className='card-footer'>
+                                  <div className="edit-trip py-1">
+                                    {auth.user && auth.user.appUserId && (
+                                    <Link className="btn btn-primary btn-sm me-1" to={`/trip/edit/${trip.tripId}`}>
+                                        <i className="bi bi-pencil-square"></i> Edit Trip
+                                    </Link>
+                                    )}
+                                    {auth.user && auth.user.hasRole('ROLE_ADMIN') && (
+                                    <button className="btn btn-danger btn-sm" onClick={() => handleDeleteTrip(trip)}>
+                                        <i className="bi bi-trash"></i> Delete Trip
+                                    </button>
+                                    )}
+                                  </div>
+                                </div> 
+                            </div>
+                    </div>   
+                ))}
+            </div>
+        
+        
     )
 }
 
